b5c4fbccd4dd2e6ae9c4c2df378b7d5cf3179fbc
Isa <isaiascedillo1313@gmail.com>
2026-06-01 11:04:34 -0600

    Creacion de MV//Bitacora

---
+++ PrototipoEFP32026/src/main/java/com/mycompany/prototipoefp32026/PrototipoEFP32026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prototipoefp32026;
+
+/**
+ *
+ * @author isaia
+ */
+public class PrototipoEFP32026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
